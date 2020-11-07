4515edffe12382aa25868f48f756ea06461185d3
parelkobra <ruiz.6789@gmail.com>
2020-11-08 00:49:29 +0100

    auto-commit

--- clientjs/src/services/noteService.js
+++
@@ -1,7 +0,0 @@
-const baseUrl = process.env.REACT_APP_BASE_URL;
-
-export default async function getNotes() {
-  const response = await fetch(`${baseUrl}notes`);
-  const notes = await response.json();
-  return notes;
-}
